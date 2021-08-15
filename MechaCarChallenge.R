253eecdd8964b4f842349de178694d254053117a
Noelle Garza <derossogarza@gmail.com>
2021-08-14 22:08:14 -0500

    Deliverable 2 complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,9 +4,20 @@ library(dplyr)
 
 # Import data
 MechaCar_df<-read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
+sps_coil<-read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
 
 # Create linear regression model
 MechaCar_lm<- lm(mpg~vehicle_weight+vehicle_length+spoiler_angle+
                    ground_clearance+AWD,data = MechaCar_df)
 # Statistical metrics from regression model
 summary(MechaCar_lm)
+
+# Create statistic summary dataframe from sps_coil PSI data
+total_summary<-sps_coil %>% 
+  summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance_PSI=var(PSI),SD_PSI=sd(PSI),.groups ='keep')
+
+# Create statistic summary to group by manufacturing lots
+lot_summary<-sps_coil%>%group_by(Manufacturing_Lot)%>%
+  summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance_PSI=var(PSI),SD_PSI=sd(PSI),.groups ='keep')
+
+
